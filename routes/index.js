703ff92bc03cf036f24a58fea050c1e6bc5fde52
marcin <ma0pla@gmail.com>
2014-11-11 22:57:25 +0100

    Logowanie i rejestracja

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,106 @@
 var express = require('express');
 var router = express.Router();
 
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code,
+    BSON = require('mongodb').pure().BSON,
+    assert = require('assert');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' })
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+        	title: 'User list',
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var userPassword = req.body.userpassword;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+        "password" : userPassword
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+
+/* GET Sign In page. */
+router.get('/signin', function(req, res) {
+    res.render('signin', { title: 'Sign In' });
+});
+
+/* POST to Verify Sign In Service */
+router.post('/profile', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    collection.find({"email" : userEmail, "password" : userPassword},function(e,docs){
+        res.render('profile', {
+        	title: 'Your Profile',
+            "profile" : docs
+        });
+    });
+
+});
+
 
 module.exports = router;
